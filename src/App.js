f2459004b9be80a8e1dfd6e46035f512022a8504
Jung Su <wjdwjdtn92@naver.com>
2023-01-02 00:01:22 +0900

    init: 기본 세팅 배포

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddUserForm from "./components/AddUser/AddUserForm";
+
+function App() {
+  return (
+    <div>
+      <AddUserForm />
+    </div>
+  );
+}
+
+export default App;
