c10eda56eff641e164ee44b432ad66565ec3edd9
Jung Su <wjdwjdtn92@naver.com>
2023-01-02 22:31:59 +0900

    feat: error modal 기능 추가

--- src/components/AddUser/AddUserForm.js
+++ src/components/AddUser/AddUserForm.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUserForm.module.css";
 
 function AddUserForm({ addUser }) {
   const [username, setUsername] = useState("");
   const [year, setYear] = useState("");
+  const [error, setError] = useState();
 
   const handleChageUsername = (event) => {
     const newName = event.target.value;
@@ -21,6 +23,10 @@ function AddUserForm({ addUser }) {
     event.preventDefault();
 
     if (username.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
 
@@ -37,8 +43,19 @@ function AddUserForm({ addUser }) {
     console.log(userData);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <Card>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <form className={styles.form} onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
         <input
